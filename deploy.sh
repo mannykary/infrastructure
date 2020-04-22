04dc029b97e86a757ace2e377446519ecc19ebb5
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-04-21 21:16:46 -0700

    Update deploy.sh to be more reusable

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,42 @@
 #! /usr/bin/env bash
-export AWS_PROFILE=$1
+
+USAGE="
+Usage:
+  PLAN:  ./deploy.sh plan <fp_context> <domain>
+  APPLY: ./deploy.sh apply <fp_context> <domain>
+"
+
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+  echo "$USAGE"
+  exit 1
+fi
+
+
+action=$1
+fp_context=$2
+domain=$3
+export TF_VAR_fp_context=$fp_context
+export TF_VAR_domain=$domain
+
+. $fp_context.env
+
+cat << EOF > backend.tf
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "fp-${fp_context}-terraform-state"
+    key    = "infrastructure.tfstate"
+  }
+}
+EOF
+
 terraform init
-terraform apply -auto-approve -var fp_context=$1 -var domain=$2
+
+if [[ "$action" == "plan" ]]; then
+  terraform validate
+  terraform plan
+elif [[ "$action" == "apply" ]]; then
+  terraform apply -auto-approve
+else
+  echo "Invalid action $action"
+fi
