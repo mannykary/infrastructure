0805947ad4b48471e79925e1534474af8eb0f216
Manny Karyampudi <mannykary@users.noreply.github.com>
2020-05-02 11:03:07 -0700

    Remove DocumentDB cluster

--- parameters.tf
+++ parameters.tf
@@ -1,21 +1,3 @@
-resource "aws_ssm_parameter" "db_host" {
-  name = "/fp/database/host"
-  type = "String"
-  value = aws_docdb_cluster.main.endpoint
-}
-
-resource "aws_ssm_parameter" "db_user" {
-  name = "/fp/database/user"
-  type = "String"
-  value = aws_docdb_cluster.main.master_username
-}
-
-resource "aws_ssm_parameter" "db_password" {
-  name = "/fp/database/password"
-  type = "SecureString"
-  value = random_password.db.result
-}
-
 resource "aws_ssm_parameter" "domain" {
   name = "/fp/domain"
   type = "String"
